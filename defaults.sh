e1b99b575bf6d5d4d66be6233345b89b89690386
thomasvn <thomasvn.dev@gmail.com>
2025-07-28 20:47:59 -0700

    Daily shutdowns

--- defaults.sh
+++ defaults.sh
@@ -7,3 +7,10 @@ killall SystemUIServer
 # Dock
 defaults write com.apple.dock autohide -bool true
 killall Dock
+
+# Daily shutdown at 23:59:00
+sudo pmset repeat shutdown MTWRFSU 22:00:00
+
+# Checking and removing schedules
+# $ pmset -g sched
+# $ sudo pmset repeat cancel
